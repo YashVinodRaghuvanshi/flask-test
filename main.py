d07584c5c19a3b3fd86766e8922aaf6f0a0db827
Yash Vinod Raghuvanshi <108932911+YashVinodRaghuvanshi@users.noreply.github.com>
2023-02-26 09:49:30 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
-from flask import Flask, jsonify
-import os
+import numpy as np
+from flask import Flask, request, jsonify
+import pickle
 
+model = pickle.load(open('regress.pkl', 'rb'))
 app = Flask(__name__)
 
-
 @app.route('/')
-def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+def home():
+    return 'Hello World'
 
+
+@app.route('/yash', methods= ['POST'])
+def predict():
+    experience = request.form.get('exp')
+    input_query = np.array([[experience]], dtype=float)
+
+    result  = model.predict(input_query)[0]
+    return jsonify({"Salary":str(result)})
 
 if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+    app.run(debug=True)
